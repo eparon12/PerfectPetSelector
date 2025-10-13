2780ba5899c254744bd21d3270c34db8ab60ffa3
eparon12 <eparon.mc@gmail.com>
2025-10-13 18:24:18 +0000

    fesf

--- PerfectPet.java
+++ PerfectPet.java
@@ -1,1 +1,21 @@
+import java.util.*;
+public class PetSelector{
+    public boolean checkForSpecial(String word) {
+        // final Char[]  _specialChars = new Char[] {";", " ", "-", "_", "*", "!", "@", "#", "$", "%", " ^"}
+        word = word.toLowerCase();
+        for(i = 0; i <= word.length(); i++){
+            if ((int)word.charAt(i) < 97 || (int)word.charAt(i) > 122){
+                
+                return false;
+                break;
+            }
+        }
+        return true;
+    }
+    public static void main(String [] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Whats is your favorite color?");
+        String favColor = scan.nextLine();
 
+    }
+}
