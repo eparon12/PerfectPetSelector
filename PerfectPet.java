0bb24e8b170d08fc1dbcde74fe55ea73973b65e5
eparon12 <eparon.mc@gmail.com>
2025-10-14 12:24:22 +0000

    dawdawd

--- PerfectPet.java
+++ PerfectPet.java
@@ -6,16 +6,58 @@ public class PetSelector{
         for(i = 0; i <= word.length(); i++){
             if ((int)word.charAt(i) < 97 || (int)word.charAt(i) > 122){
                 
-                return false;
+                return true;
                 break;
             }
         }
-        return true;
+        return false;
     }
     public static void main(String [] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Whats is your favorite color?");
         String favColor = scan.nextLine();
+        favColor = favColor.toLowerCase();
+        if(favColor.checkForSpecial(favColor)){
+            System.out.println("Invalid Color.");
+            break;
+        }
+        else{
+            switch (favColor){
+                case "blue":
+                    blueColor();
+                case "red":
+                    redColor();
+                case "green":
+                    greenColor();
+                default:
+                    colorNotQual();
+            }
+
+            
+            
+        }
+        scan.close();
+
+    }
+    public static void blueColor(String [] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("What is your favorite season? ");
+        String favSeason = scan.nextLine();
+        favSeason = favSeason.toLowerCase();
+        if(favSeason.checkForSpecial(favSeason)){
+            System.out.println("Invalid Season.");
+            break;
+        }
+        switch(favSeason){
+            case "fall":
+            
+            case "autumn":
+            case "summer":
+            case "winter":
+            default:
+                System.out.println("Not a valid season.");
+                break;
+        }
 
     }
 }
