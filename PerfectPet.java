2e2c559db89cfb73292ac7ae63035f3e60e7090b
eparon12 <eparon.mc@gmail.com>
2025-10-14 18:29:15 +0000

    do color check

--- PerfectPet.java
+++ PerfectPet.java
@@ -1,6 +1,7 @@
 import java.util.*;
 public class PetSelector{
-    public boolean checkForSpecial(String word) {
+    
+    public static boolean checkForSpecial(String word) {
         // final Char[]  _specialChars = new Char[] {";", " ", "-", "_", "*", "!", "@", "#", "$", "%", " ^"}
         word = word.toLowerCase();
         for(i = 0; i <= word.length(); i++){
@@ -13,24 +14,26 @@ public class PetSelector{
         return false;
     }
     public static void main(String [] args) {
+        String pet;
         Scanner scan = new Scanner(System.in);
         System.out.println("Whats is your favorite color?");
         String favColor = scan.nextLine();
         favColor = favColor.toLowerCase();
-        if(favColor.checkForSpecial(favColor)){
+        if(checkForSpecial(favColor)){
             System.out.println("Invalid Color.");
             break;
         }
         else{
             switch (favColor){
                 case "blue":
-                    blueColor();
+                    pet = blueColor();
                 case "red":
                     redColor();
                 case "green":
                     greenColor();
                 default:
-                    colorNotQual();
+                    System.out.println("Only color options are blue red or green");
+                    break;
             }
 
             
@@ -39,21 +42,79 @@ public class PetSelector{
         scan.close();
 
     }
-    public static void blueColor(String [] args){
+        public static String greenColor(String [] args){
         Scanner scan = new Scanner(System.in);
         System.out.println("What is your favorite season? ");
         String favSeason = scan.nextLine();
         favSeason = favSeason.toLowerCase();
-        if(favSeason.checkForSpecial(favSeason)){
+        if(checkForSpecial(favSeason)){
             System.out.println("Invalid Season.");
             break;
         }
         switch(favSeason){
             case "fall":
-            
-            case "autumn":
+                return "rock";
+            case "spring":
+                return "dog";
             case "summer":
+                return "dog";
             case "winter":
+                if(!"aeiou".contains(name.substring(0, 1))){
+                    return "giraffe";
+                    boolean giraffe = true;
+                }
+                else{
+                    return "rock";
+                }
+            default:
+                System.out.println("Not a valid season.");
+                break;
+        }
+    }
+    public static String redColor(String [] args){
+        Scanner scan = new Scanner(System.in);
+        if("aeiou".contains(name.substring(0, 1))){
+            return "porcupine";
+        }
+        else{
+            return "panda";
+        }
+        
+    }
+    public static String blueColor(String [] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("What is your favorite season? ");
+        String favSeason = scan.nextLine();
+        favSeason = favSeason.toLowerCase();
+        if(checkForSpecial(favSeason)){
+            System.out.println("Invalid Season.");
+            break;
+        }
+        switch(favSeason){
+            case "fall":
+                return "alligator";
+            case "spring":
+                return "ostrich";
+            case "summer":
+                return "ponie";
+            case "winter":
+                System.out.println("Whats your name?");
+                String name = scan.nextLine();
+                name = name.toLowerCase();
+                if(checkForSpecial(name)){
+                    System.out.println("Thats not a name");
+                    break;
+                }
+                else{
+                    //if(name.substring(0,1) != "a" || name.substring(0,1) != "e" || name.substring(0,1) != "o" || name.substring(0,1) !="u" || name.substring(0,1) != "i" ){}
+                    if("aeiou".contains(name.substring(0, 1))){
+                        return "rock";
+                    }
+                    else{
+                        return "axolotl";
+                    }
+                }
+                
             default:
                 System.out.println("Not a valid season.");
                 break;
